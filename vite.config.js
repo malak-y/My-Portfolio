eac23c4d6ab51483f9668fd76d3a8abd8dc4d20e
malak-y <145293320+malak-y@users.noreply.github.com>
2024-08-31 16:59:35 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/', // Replace 'your-repository-name' with the actual name of your GitHub repository
+  base: '/My-Portfolio/', // Replace with your GitHub repository name
 });
